dcf8514c5a84090b5d8faf09f925dd0622acd62b
Rickzzz <146898598+rickzz0@users.noreply.github.com>
2024-09-17 12:50:48 -0300

    Add files via upload

---
+++ dirbrute.py
@@ -0,0 +1,15 @@
+import requests
+
+
+url = input("Digite a url: ")
+
+with open("wordlist.txt", "r") as file:
+        wordlist = file.readlines()
+
+for word in wordlist:
+	url_final = "{}/{}".format(url, word.strip())
+	response = requests.get(url_final)
+	code = response.status_code
+	if code != 404:
+		print("{} --> {}".format(url_final, code))
+
